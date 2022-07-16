c9bd7110602b0977d633306696b1abc5f156de6a
JS0909 <zejace3@gmail.com>
2022-07-17 00:33:56 +0900

    jena

--- keras/keras45_lstm_kaggle_jena.py
+++ keras/keras45_lstm_kaggle_jena.py
@@ -12,15 +12,17 @@ import pandas as pd
 
 # 1. 데이터
 path = './_data/kaggle_jena/'
-datasets = pd.read_csv(path + 'jena_climate_2009_2016.csv')
+dataset = pd.read_csv(path + 'jena_climate_2009_2016.csv')
 
 # 데이터 정규화
 scaler = MinMaxScaler()    
-scale_col = ["p (mbar)","T (degC)","Tpot (K)","Tdew (degC)","rh (%)","VPmax (mbar)",
+scale_cols = ["p (mbar)","T (degC)","Tpot (K)","Tdew (degC)","rh (%)","VPmax (mbar)",
              "VPact (mbar)","VPdef (mbar)","sh (g/kg)","H2OC (mmol/mol)",
-             "rho (g/m**3)","wv (m/s)","max. wv (m/s)","wd (deg)"]
+             "rho (g/m**3)","wv (m/s)","max. wv (m/s)","wd (deg)"] # 정규화 대상 칼럼 (Date Time 제외한 나머지 숫자 칼럼)
+scaled_dataset = scaler.fit_transform(dataset[scale_cols]) # 정규화 수행
+scaled_dataset = pd.DataFrame(scaled_dataset, columns = scale_cols) # 데이터프레임화
 
-# print(datasets.info())
+# print(dataset.info())
 # <class 'pandas.core.frame.DataFrame'>
 # RangeIndex: 420551 entries, 0 to 420550
 # Data columns (total 15 columns):
@@ -43,19 +45,19 @@ scale_col = ["p (mbar)","T (degC)","Tpot (K)","Tdew (degC)","rh (%)","VPmax (mba
 #  14  wd (deg)         420551 non-null  float64
 # dtypes: float64(14), object(1)
 
-datasets['Date Time'] = pd.to_datetime(datasets['Date Time'])
-datasets['year'] = datasets['Date Time'].dt.strftime('%Y') 
-datasets['month'] = datasets['Date Time'].dt.strftime('%m')
-datasets['hour'] = datasets['Date Time'].dt.strftime('%H')
-datasets['date'] = datasets['Date Time'].dt.strftime('%d')
-datasets = datasets.drop(['Date Time'], axis=1)
+dataset['Date Time'] = pd.to_datetime(dataset['Date Time'])
+dataset['year'] = dataset['Date Time'].dt.strftime('%Y') 
+dataset['month'] = dataset['Date Time'].dt.strftime('%m')
+dataset['hour'] = dataset['Date Time'].dt.strftime('%H')
+dataset['date'] = dataset['Date Time'].dt.strftime('%d')
+dataset = dataset.drop(['Date Time'], axis=1)
 
 cols = ['month', 'date', 'year', 'hour']
 for col in cols:
     le = LabelEncoder()
-    datasets[col] = le.fit_transform(datasets[col])
+    dataset[col] = le.fit_transform(dataset[col])
 
-# datasets.shape (n, 18)
+# dataset.shape (n, 18)
 
 def split_x(dataset, size):
     aaa = []
@@ -64,13 +66,19 @@ def split_x(dataset, size):
         aaa.append(subset) # 하나하나 리스트 형태로 추가함
     return np.array(aaa)
 
-bbb = split_x(datasets, 5)
+bbb = split_x(dataset, 5)
 # print(bbb)
 print(bbb.shape) # (420547, 5, 18)
+print(bbb)
+# bbb = pd.DataFrame(bbb, columns=['month', 'date', 'year', 'hour', "p (mbar)","T (degC)","Tpot (K)","Tdew (degC)","rh (%)",
+#                                  "VPmax (mbar)","VPact (mbar)","VPdef (mbar)","sh (g/kg)","H2OC (mmol/mol)",
+#                                  "rho (g/m**3)","wv (m/s)","max. wv (m/s)","wd (deg)"])
+# 오류 메세지 Must pass 2-d input. shape=(420547, 5, 18)
 
+# 인덱스 슬라이싱 찾아서 수정하자...
+# x =  bbb[]
+# y =  bbb[]
 
-x =  bbb[:, :-1]
-y =  bbb[:, -1]
 # print(x, y)
 print(x.shape, y.shape) # (420547, 4, 15) (420547, 15)
 
@@ -91,11 +99,30 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 start_time = time.time()
 Es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=500, restore_best_weights=True)
-model.fit(x_train, y_train, epochs=1000, callbacks=[Es], validation_split=0.1)
+fit_log = model.fit(x_train, y_train, epochs=1, callbacks=[Es], validation_split=0.1)
 end_time = time.time()
 
 # 4. 평가, 예측
 loss = model.evaluate(x_test, y_test)
+predict = model.predict(x_test)
 print('loss: ', loss)
+print('prdict: ', predict)
+
+# 그래프
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rcParams['font.family'] = 'malgun gothic'
+mpl.rcParams['axes.unicode_minus'] = False
+
+plt.figure(figsize=(12,6))
+plt.plot(y_test, label = 'actual', c = 'red')
+plt.plot(predict, c='blue', label='val_loss')
+plt.plot(loss, markers = '.', c = 'green', label = 'loss')
+plt.grid()
+plt.title('예나 온도 예측')
+plt.ylabel('온도')
+plt.xlabel('날짜')
+plt.legend()
+plt.show()
 
 # loss:  141180.5
