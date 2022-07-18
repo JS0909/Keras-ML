69d8ebf85394cbaf133c9d493ddc6178efe164f4
JS0909 <zejace3@gmail.com>
2022-07-18 14:55:55 +0900

    commit

--- keras/keras46_amore1_kjs_save.py
+++ keras/keras46_amore1_kjs_save.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sklearn import datasets
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, GRU, Conv1D, Flatten, Reshape
+from tensorflow.python.keras.models import Sequential, Model
+from tensorflow.python.keras.layers import Input
+from tensorflow.python.keras.layers import Dense, GRU, Conv1D
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.callbacks import EarlyStopping
 import time
@@ -11,9 +12,13 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 # 1. 데이터
-path = './_data/stock_exam/'
-dataset_sam = pd.read_csv(path + '삼성전자220718.csv', encoding='cp949')
-dataset_amo = pd.read_csv(path + '아모레220718.csv',  encoding='cp949')
+path = './_data/test_amore_0718/'
+dataset_sam = pd.read_csv(path + '삼성전자220718.csv', thousands=',', encoding='cp949')
+dataset_amo = pd.read_csv(path + '아모레220718.csv', thousands=',', encoding='cp949')
+
+dataset_sam = dataset_sam.drop(['전일비','금액(백만)','신용비','개인','외인(수량)','프로그램','외인비'], axis=1)
+dataset_amo = dataset_amo.drop(['전일비','금액(백만)','신용비','개인','외인(수량)','프로그램','외인비'], axis=1)
+
 
 dataset_sam['일자'] = pd.to_datetime(dataset_sam['일자'], format='%Y/%m/%d')
 dataset_sam['연도']=dataset_sam['일자'].dt.year
@@ -31,11 +36,81 @@ plt.xlabel('time')
 plt.ylabel('price')
 plt.show()
 '''
+# dataset_amo.info()
+# dataset_sam.info()
+dataset_sam = dataset_sam.dropna()
+dataset_amo = dataset_amo.dropna()
 
-dataset_amo.sort_index(ascending=False).reset_index(drop=True)
+dataset_sam = dataset_sam.loc[dataset_sam['일자']>="2018/05/04"] # 액면분할 이후 데이터만 사용
+dataset_amo = dataset_sam.loc[dataset_sam['일자']>="2018/05/04"] # 삼성의 액면분할 날짜 이후의 행개수에 맞춰줌
+print(dataset_amo.shape, dataset_sam.shape) # (1035, 11) (1035, 11)
 
+# dataset_amo.sort_index(ascending=False).reset_index(drop=True)
+# 데이터 스케일링
 scaler = MinMaxScaler()
 scale_cols = ['시가', '고가', '저가', '종가', '거래량', '기관', '외국계']
-df_scaled = scaler.fit_transform(dataset_amo[scale_cols])
-df_scaled = pd.DataFrame(df_scaled)
-df_scaled.columns = scale_cols
+df_scaled_a = scaler.fit_transform(dataset_amo[scale_cols])
+df_scaled_a = pd.DataFrame(df_scaled_a)
+df_scaled_a.columns = scale_cols
+
+df_scaled_s = scaler.fit_transform(dataset_sam[scale_cols])
+df_scaled_s = pd.DataFrame(df_scaled_s)
+df_scaled_s.columns = scale_cols
+
+feature_cols = ['시가', '고가', '저가', '거래량', '기관', '외국계', '종가']
+label_cols = ['시가']
+
+# 시계열 데이터 만드는 함수
+def split_x(dataset, size):
+    aaa = []
+    for i in range(len(dataset) - size + 1):
+        subset = dataset[i : (i + size)]
+        aaa.append(subset)
+    return np.array(aaa)
+
+SIZE = 20
+x1 = split_x(df_scaled_a[feature_cols], SIZE)
+y = split_x(df_scaled_a[label_cols], SIZE)
+x2 = split_x(df_scaled_s[feature_cols], SIZE)
+print(x1.shape, x2.shape, y.shape) # (1016, 20, 7) (1016, 20, 7) (1016, 20, 1)
+
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2, shuffle=False)
+# x1_train, x1_test, y_train, y_test = train_test_split(x1, y, test_size=0.2, shuffle=False)
+
+# 2. 모델구성
+# 2-1. 모델1
+input1 = Input(shape=(20,7))
+dense1 = Conv1D(64, 2, activation='relu', name='d1')(input1)
+dense2 = GRU(128, activation='relu', name='d2')(dense1)
+dense3 = Dense(64, activation='relu', name='d3')(dense2)
+output1 = Dense(32, activation='relu', name='out_d1')(dense3)
+
+# 2-2. 모델2
+input2 = Input(shape=(20, 7))
+dense11 = Conv1D(64, 2, activation='relu', name='d11')(input2)
+dense12 = GRU(128, activation='swish', name='d12')(dense11)
+dense13 = Dense(64, activation='relu', name='d13')(dense12)
+dense14 = Dense(32, activation='relu', name='d14')(dense13)
+output2 = Dense(16, activation='relu', name='out_d2')(dense14)
+
+from tensorflow.python.keras.layers import concatenate
+merge1 = concatenate([output1, output2], name='m1')
+merge2 = Dense(100, activation='relu', name='mg2')(merge1)
+merge3 = Dense(100, name='mg3')(merge2)
+last_output = Dense(1, name='last')(merge3)
+
+model = Model(inputs=[input1, input2], outputs=[last_output])
+
+# 3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+start_time = time.time()
+Es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=500, restore_best_weights=True)
+fit_log = model.fit([x1_train, x2], y_train, epochs=1, batch_size=64, callbacks=[Es], validation_split=0.1)
+end_time = time.time()
+
+# 4. 평가, 예측
+loss = model.evaluate([x1_test, x2], y_test)
+predict = model.predict([x1_test, x2])
+print('loss: ', loss)
+print('prdict: ', predict)
+print('걸린 시간: ', end_time-start_time)
