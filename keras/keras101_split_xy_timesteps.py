9540bef3da3c4f00ab8f59be1f51990b7212758d
JS0909 <zejace3@gmail.com>
2022-07-24 12:59:46 +0900

    commit 220724

--- keras/keras101_split_xy_timesteps.py
+++ keras/keras101_split_xy_timesteps.py
@@ -4,9 +4,7 @@ dataset = [[1, 2, 3, 4, 5],
             [6, 7, 8, 9, 10],
             [11, 12, 13, 14, 15]]
 
-
 dataset = np.array(dataset) 
-
 print(dataset.shape) # (3, 5)
 
 def split_xy3(dataset, time_steps, y_column):
@@ -15,15 +13,15 @@ def split_xy3(dataset, time_steps, y_column):
         x_end_number = i + time_steps
         y_end_number = x_end_number + y_column -1
         
-        if y_end_number > len(dataset):
+        if y_end_number > len(dataset): # y짜르는 곳이 데이터셋 길이 벗어나면 멈춤
             break
-        tmp_x = dataset[i:x_end_number, : -1]
-        tmp_y = dataset[x_end_number-1: y_end_number, -1]
+        tmp_x = dataset[i:x_end_number, : -1] # 현재반복번째 ~ 현재반복번째+타임스텝까지, 전체를 다 저장
+        tmp_y = dataset[x_end_number-1: y_end_number, -1] # 현재반복번째+타임스텝-1 ~ 현재반복번째+y칼럼수(y지정일 수)-1, 제일 마지막 행만 저장
         x.append(tmp_x)
         y.append(tmp_y)
     return np.array(x), np.array(y)
 
 x, y = split_xy3(dataset, 1, 2)
 
-print(x.shape, y.shape)
+print(x.shape, y.shape) # (2, 1, 4) (2, 2)
 print(x,'\ny: ', y)
