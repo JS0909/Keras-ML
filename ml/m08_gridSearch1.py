87f7ca3db32c70700e48766233e9d95dd6bc8629
JS0909 <zejace3@gmail.com>
2022-08-05 11:48:46 +0900

    commit

--- ml/m08_gridSearch1.py
+++ ml/m08_gridSearch1.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, KFold, cross_val_score,\
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score,\
     GridSearchCV # 격자 탐색, CV: cross validation
 
 # 1. 데이터
@@ -26,17 +26,16 @@ print(y.shape) # (150,)
 print('y의 라벨값: ', np.unique(y))
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8,shuffle=True, random_state=9)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=9)
 
 n_splits = 5
-kfold = KFold(n_splits=n_splits, shuffle=True, random_state=9)
+kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=9)
 
 parameters = [
-    {'c':[1,10,100,1000], 'kernel':['linear'], 'degree':[3,4,5]},
-    {'c':[1,10,100], 'kernel':['rbf'], 'gamma':[0.001, 0.0001]},
-    {'c':[1,10,100,1000], 'kernel':['sigmoid'],
-     'gamma':[0.01,0.001,0.0001], 'degree':[3,4]}
-]
+    {'C':[1,10,100,1000], 'kernel':['linear'], 'degree':[3,4,5]},                                # 12
+    {'C':[1,10,100], 'kernel':['rbf'], 'gamma':[0.001, 0.0001]},                                 # 6
+    {'C':[1,10,100,1000], 'kernel':['sigmoid'], 'gamma':[0.01,0.001,0.0001], 'degree':[3,4]}     # 24
+]                                                                                                # 총 42회 파라미터 해봄
                       
 #2. 모델구성
 from sklearn.svm import LinearSVC, SVC
@@ -45,19 +44,29 @@ from sklearn.neighbors import KNeighborsClassifier # 최근접 이웃
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier # 결정트리를 여러개 랜덤으로 뽑아서 앙상블해서 봄
 
-models = [LinearSVC, SVC, Perceptron, LogisticRegression, KNeighborsClassifier, DecisionTreeClassifier, RandomForestClassifier]
+# model = SVC(C=1, kernel='linear', degree=3)
+model = GridSearchCV(SVC(), parameters, cv=kfold, verbose=1, refit=True, n_jobs=-1)
+# refit: True면 최적의 파라미터로 훈련, False면 해당 파라미터로 훈련하지 않고 마지막 파라미터로 훈련
+# n_jobs: cpu의 갯수를 몇개 사용할것인지
 
-for model in models:
-    model = model()
-    model_name = str(model).strip('()')
-    model.fit(x_train, y_train)
-    result = model.score(x_test, y_test)
-    print(model_name, '결과: ', result)
-    
-# LinearSVC 결과:  1.0
-# SVC 결과:  1.0
-# Perceptron 결과:  1.0
-# LogisticRegression 결과:  1.0
-# KNeighborsClassifier 결과:  1.0
-# DecisionTreeClassifier 결과:  1.0
-# RandomForestClassifier 결과:  1.0
+# 3. 컴파일, 훈련
+model.fit(x_train, y_train)
+
+print('최적의 매개변수: ', model.best_estimator_)
+# 최적의 매개변수:  SVC(C=10, kernel='linear')
+print('최적의 파라미터: ', model.best_params_)
+# 최적의 파라미터:  {'C': 10, 'degree': 3, 'kernel': 'linear'}
+print('best_score_: ', model.best_score_)
+# best_score_:  0.975
+print('model.score: ', model.score(x_test, y_test))
+# model.score:  1.0
+
+ypred = model.predict(x_test)
+print('acc score: ', accuracy_score(y_test, ypred))
+# acc score:  1.0
+
+ypred_best = model.best_estimator_.predict(x_test)
+print('best tuned acc: ', accuracy_score(y_test, ypred_best))
+# best tuned acc:  1.0
+
+# score / acc score / best_estimator_ : 모두 같음, 즉 자동으로 제일 좋은 것을 골라 써준다
