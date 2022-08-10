4971e5006b0046d4e5d7f96893aa1eb0794a96f5
JS0909 <zejace3@gmail.com>
2022-08-10 20:34:12 +0900

    commit

---
+++ ml/m100_import_test.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+aaa = np.array([[-10,2,3,4,5,6,7,8,9,10,11,12,50],
+                [100,200,-30,400,500,600,-70000,800,900,1000,210,420,350]])
+aaa = np.transpose(aaa)
+print(aaa.shape)
+print(aaa)
+
+import m32_outlier2 as m32
+
+m32.outliers_printer(aaa)
