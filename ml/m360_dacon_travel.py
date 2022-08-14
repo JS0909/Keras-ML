4ca925afce91fe57c3dd89b7079bdeb7ac7ba8ff
JS0909 <zejace3@gmail.com>
2022-08-14 21:03:21 +0900

    commit

--- ml/m360_dacon_travel.py
+++ ml/m360_dacon_travel.py
@@ -16,9 +16,12 @@ from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import train_test_split, StratifiedKFold,\
     HalvingRandomSearchCV, RandomizedSearchCV, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer, KNNImputer
+
 
 # 1. 데이터
-filepath = 'D:\study_data\_data\dacon_travel/'
+filepath = 'D:\study_home\_data\dacon_travel/'
 train = pd.read_csv(filepath+'train.csv', index_col=0)
 test = pd.read_csv(filepath+'test.csv', index_col=0)
 
@@ -60,14 +63,16 @@ for i in idxarr:
 # ------------------------------------------
 
 # 피처임포턴스 그래프 보기 위해 데이터프레임형태의 x_, y_ 놔둠 / 훈련용 넘파이어레이형태의 x, y 생성-----------
-x_ = train.drop(['ProdTaken','NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar'], axis=1) # 피처임포턴스로 확인한 중요도 낮은 탑3 제거
+# x_ = train.drop(['ProdTaken','NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar'], axis=1) # 피처임포턴스로 확인한 중요도 낮은 탑3 제거
+x_ = train.drop(['ProdTaken','NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar', 'MonthlyIncome'], axis=1)
 # x_ = train.drop(['ProdTaken'], axis=1)
 y_ = train['ProdTaken']
 x = np.array(x_)
 y = np.array(y_)
 y = y.reshape(-1, 1) # y값 reshape 해야되서 x도 넘파이로 바꿔 훈련하는 것
 
-test = test.drop(['NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar'], axis=1) # 피처임포턴스로 확인한 중요도 낮은 탑3 제거
+# test = test.drop(['NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar'], axis=1) # 피처임포턴스로 확인한 중요도 낮은 탑3 제거
+test = test.drop(['NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar', 'MonthlyIncome'], axis=1)
 test = np.array(test)
 # print(x.shape, y.shape)
 #-----------------------------------------------------------------------------------------------------------
@@ -86,19 +91,81 @@ def outliers(data_out):
     return np.where((data_out>upper_bound) | (data_out<lower_bound))
 
 def outliers_printer(dataset):
-    plt.figure(figsize=(10,8))
+    plt.figure(figsize=(10,13))
     for i in range(dataset.shape[1]):
         col = dataset[:, i]
         outliers_loc = outliers(col)
         print(i, '열의 이상치의 위치: ', outliers_loc, '\n')
         plt.subplot(math.ceil(dataset.shape[1]/2),2,i+1)
         plt.boxplot(col)
+        plt.title(i)
         
     plt.show()
 
-outliers_printer(x)
+# outliers_printer(x)
 #------------------------------------------------------------------
 '''
+
+a3 =  [  89,  110,  120,  121,  137,  147,  172,  210,  211,  247,  310,
+        365,  397,  398,  426,  448,  450,  567,  755,  802,  865,  935,
+       1033, 1036, 1100, 1136, 1237, 1241, 1387, 1413, 1435, 1447, 1449,
+       1461, 1471, 1521, 1540, 1652, 1665, 1722, 1782, 1919, 1936, 1947]
+
+a6 = [   8,   10,   13,   33,   40,   53,   80,   85,  108,  127,  129,
+        150,  151,  172,  178,  199,  206,  214,  233,  254,  256,  281,
+        327,  335,  344,  355,  364,  367,  380,  420,  426,  438,  462,
+        496,  498,  512,  575,  610,  631,  689,  711,  714,  718,  726,
+        757,  761,  768,  777,  778,  790,  816,  827,  829,  834,  840,
+        857,  871,  875,  904,  913,  946,  960,  971,  972,  994, 1007,
+       1009, 1021, 1024, 1060, 1077, 1081, 1098, 1099, 1121, 1124, 1142,
+       1182, 1208, 1237, 1245, 1259, 1289, 1296, 1314, 1322, 1337, 1344,
+       1348, 1362, 1365, 1377, 1389, 1400, 1422, 1436, 1473, 1505, 1558,
+       1591, 1598, 1606, 1610, 1621, 1631, 1641, 1677, 1715, 1724, 1726,
+       1750, 1766, 1817, 1825, 1852, 1858, 1873, 1881, 1887, 1925, 1930]
+
+a10 = [  90,  106,  129,  161,  177,  248,  302,  303,  316,  348,  425,
+        552,  623,  698,  769,  873,  927,  987, 1055, 1076, 1175, 1325,
+       1360, 1404, 1425, 1514, 1525, 1598, 1611, 1663, 1731, 1749, 1753,
+       1768, 1828, 1844, 1853, 1933]
+
+a13 = [  14,   59,   93,  105,  142,  167,  187,  203,  209,  218,  230,
+        250,  265,  311,  314,  322,  342,  378,  447,  479,  503,  536,
+        570,  592,  643,  662,  727,  729,  749,  768,  817,  819,  827,
+        851,  852,  869,  912,  917,  944,  964,  995, 1020, 1097, 1101,
+       1110, 1121, 1123, 1156, 1157, 1203, 1277, 1311, 1316, 1336, 1355,
+       1363, 1380, 1398, 1417, 1474, 1486, 1502, 1507, 1518, 1526, 1536,
+       1561, 1571, 1576, 1578, 1597, 1640, 1643, 1668, 1676, 1716, 1719,
+       1739, 1750, 1783, 1791, 1818, 1822, 1823, 1856, 1870, 1887, 1899,
+       1927]
+
+for i in range(len(a3)):
+    x[a3[i]][3] = 20
+
+# x[485][4] = np.nan # Occupation
+
+# for i in range(len(a6)): # ProductPitched
+#     x[a6[i]][6] = np.nan
+    
+for i in range(len(a10)): # NumberOfTrips
+    x[a10[i]][10] = np.nan
+
+for i in range(len(a13)): # Designation
+    x[a13[i]][13] = np.nan
+    
+# outliers_printer(x)
+
+x = pd.DataFrame(x, columns=[x_.columns])
+
+# x['Occupation'].fillna(x['Occupation'].median, inplace=True)
+x['ProductPitched'].fillna(x['ProductPitched'].median, inplace=True)
+x['Designation'].fillna(x['Designation'].median, inplace=True)
+x['NumberOfTrips'].fillna(x['NumberOfTrips'].median, inplace=True)
+
+ipt = IterativeImputer(max_iter = 100, random_state = 999)
+# ipt = KNNImputer()
+x = ipt.fit_transform(x)
+x = np.array(x)
+
 
 ''' PCA 반복문 - XGB 테스트 // 별로 안좋게 나옴, LDA는 y라벨 두개뿐이라 큰 의미 없음
 for i in range(x.shape[1]):
@@ -124,11 +191,11 @@ parameters_xgb = {
               } 
 
 parameters_rnf = [
-    {'n_estimators':[100,200]},
-    {'max_depth':[6,8,10,12]},
-    {'min_samples_leaf':[3,5,7,10]},
-    {'min_samples_split':[2,3,5,10]},
-    {'n_jobs':[-1,2,4]}
+    {'n_estimators':[100,200,300]},
+    {'max_depth':[None,6,8,10,12]},
+    {'min_samples_leaf':[3,5,7,10,11,13]},
+    {'min_samples_split':[2,3,5,7,10]},
+    {'n_jobs':[-1]}
 ]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=999, shuffle=True)
@@ -136,12 +203,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random
 
 # 2. 모델
 xgb = XGBClassifier(tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0)
-rnf = RandomForestClassifier()
+rnf = RandomForestClassifier(random_state=704) # 704 : 0.9053708439897699
 
 # model = xgb
 # model = rnf
 # model = make_pipeline(MinMaxScaler(), HalvingRandomSearchCV(xgb, parameters_xgb, cv=5, n_jobs=-1, verbose=2))
-model = make_pipeline(MinMaxScaler(), HalvingRandomSearchCV(rnf, parameters_rnf, cv=5, n_jobs=-1, verbose=2))
+model = make_pipeline(MinMaxScaler(), HalvingRandomSearchCV(rnf, parameters_rnf, cv=5, n_jobs=-1, verbose=2, random_state=999))
+# model = make_pipeline(MinMaxScaler(), GridSearchCV(rnf, parameters_rnf, cv=5, n_jobs=-1, verbose=2))
 # model = make_pipeline(MinMaxScaler(), xgb)
 # model = make_pipeline(MinMaxScaler(), rnf)
 
@@ -166,6 +234,10 @@ plt.show()
 # NumberOfChildrenVisiting, NumberOfPersonVisiting, OwnCar 가장 영향력 적음
 # ----------------------------------------------------------------------
 '''
+
+import joblib
+joblib.dump(model,'D:\study_home\_data\dacon_travel\_dat/m360_travel6.dat')
+# model = joblib.load('D:\study_home\_data\dacon_travel\_dat/m360_travel.dat')
 
 
 # 2. 모델
@@ -248,9 +320,9 @@ print('걸린 시간: ', end-start)
 # 5. 제출 준비
 y_submit = model.predict(test)
 
-# submission = pd.read_csv(filepath+'submission.csv', index_col=0)
-# submission['ProdTaken'] = y_submit
-# submission.to_csv(filepath + 'submission.csv', index = True)
+submission = pd.read_csv(filepath+'submission.csv', index_col=0)
+submission['ProdTaken'] = y_submit
+submission.to_csv(filepath + 'submission.csv', index = True)
 
 
 
@@ -291,6 +363,42 @@ y_submit = model.predict(test)
 # 스코어:  0.8746803069053708
 # 걸린 시간:  4.406193733215332
 
+# submission 9번파일 + 월급 제외
+# 스코어:  0.8976982097186701
+# 걸린 시간:  4.571194648742676
+
+# submission 10번파일 + 월급 제외 + 3 DurationOfPitch의 이상치 제거
+# 스코어:  0.9028132992327366
+# 걸린 시간:  4.258755207061768
+
+# ProductPitched 제거
+# 스코어:  0.9002557544757033
+# 걸린 시간:  4.518233060836792
+
+# 랜포 시드 704 travel3.dat
+# 스코어:  0.9053708439897699
+# 걸린 시간:  5.0596325397491455
+
+# 12
+# 스코어:  0.9028132992327366
+# 걸린 시간:  5.500649452209473
+
+# 13 m360_travel2
+# 스코어:  0.907928388746803
+# 걸린 시간:  5.145014524459839
+
+# 14 m360_travel4 + occupation 중간값 + 랜포 704
+# 스코어:  0.907928388746803
+# 걸린 시간:  7.033788442611694
+
+# 15 m360_travel5 + occupation 놔둠 + 랜포 704
+# 스코어:  0.9053708439897699
+# 걸린 시간:  6.28637433052063
+
+# 16 m360_travel6 + ProductPitched 이상치 처리안함
+# 스코어:  0.9002557544757033
+# 걸린 시간:  6.224222183227539
+
 '''
  #   Column                    Non-Null Count  Dtype
 ---  ------                    --------------  -----
@@ -300,7 +408,7 @@ y_submit = model.predict(test)
  3   DurationOfPitch           1955 non-null   float64
  4   Occupation                1955 non-null   int32
  5   Gender                    1955 non-null   int32
- 6   NumberOfPersonVisiting    1955 non-null   int64
+#  6   NumberOfPersonVisiting    1955 non-null   int64
  7   NumberOfFollowups         1955 non-null   float64
  8   ProductPitched            1955 non-null   int32
  9   PreferredPropertyStar     1955 non-null   float64
@@ -308,9 +416,9 @@ y_submit = model.predict(test)
  11  NumberOfTrips             1955 non-null   float64
  12  Passport                  1955 non-null   int64
  13  PitchSatisfactionScore    1955 non-null   int64
- 14  OwnCar                    1955 non-null   int64
- 15  NumberOfChildrenVisiting  1955 non-null   float64
+#  14  OwnCar                    1955 non-null   int64
+#  15  NumberOfChildrenVisiting  1955 non-null   float64
  16  Designation               1955 non-null   int32
- 17  MonthlyIncome             1955 non-null   float64
+#  17  MonthlyIncome             1955 non-null   float64
  18  ProdTaken                 1955 non-null   int64
  '''
--- ml/m361_dacon_antena.py
+++ ml/m361_dacon_antena.py
@@ -5,8 +5,11 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.multioutput import MultiOutputRegressor
 from xgboost import XGBRegressor
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer, KNNImputer
 
 
 def seed_everything(seed):
@@ -15,21 +18,36 @@ def seed_everything(seed):
     np.random.seed(seed)
 seed_everything(99) # Seed 고정
 
-filepath = 'D:/study_home/_data/dacon_antena/'
+filepath = 'D:/study_data/_data/dacon_antena/'
 
-train = pd.read_csv(filepath + 'train.csv')
+train = pd.read_csv(filepath + 'train.csv',index_col=0)
 test = pd.read_csv(filepath + 'test.csv').drop(columns=['ID'])
 
 
 train_x = train.filter(regex='X') # Input : X Featrue
 train_y = train.filter(regex='Y') # Output : Y Feature
 
-train_x = train_x.drop(['X_10', 'X_11'], axis=1)
-test = test.drop(['X_10', 'X_11'], axis=1)
+cols = ["X_10","X_11"]
+train[cols] = train[cols].replace(0, np.nan)
 
-# model = MultiOutputRegressor(XGBRegressor(n_estimators=100, learning_rate=0.08, gamma = 0, subsample=0.75, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y)
-model = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
+# train[cols].fillna(train[cols].mean(), inplace=True)
 
+# imp = KNNImputer()
+
+imp = IterativeImputer(estimator = LinearRegression(), 
+                       tol= 1e-10, 
+                       max_iter=30, 
+                       verbose=2, 
+                       imputation_order='roman')
+
+train = imp.fit_transform(train)
+
+
+# model = MultiOutputRegressor(XGBRegressor(n_estimators=150, learning_rate=0.08, gamma = 1, subsample=0.75, colsample_bytree = 1, max_depth=7) )
+model = MultiOutputRegressor(LinearRegression())
+# model = RandomForestRegressor()
+
+model.fit(train_x, train_y)
 preds = model.predict(test)
 print(model.score(train_x, train_y))
 
@@ -46,3 +64,11 @@ submit.to_csv(filepath + 'submission.csv', index=False)
 # 0.2842927683724148 xg부스트 스렉이
 
 # 0.03953156092196286 칼럼 드랍 없이 / 제출 446위
+
+# 0.03932477616005312 x10, x11 칼럼 드랍 리니어
+
+# 0.8708800720214304 랜덤포레스트, 멀티아웃풋 아니라 그런가;
+
+# 0.039531560921963645 x10, x11 칼럼 결측치 처리(IterativeImputer)
+
+# 0.039531560921963645 x10, x11 칼럼 결측치 처리(KNNImputer)
