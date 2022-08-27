b2d884274e628b02343c6fe84ddbbce352d840a5
JS0909 <zejace3@gmail.com>
2022-08-27 20:54:08 +0900

    commit

---
+++ 코딩테스트_큰수만들기_갖고온거.py
@@ -0,0 +1,10 @@
+def solution(number, k):
+    stack = []
+    for num in number:
+        while stack and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
