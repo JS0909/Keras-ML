a16a89e622f47891693ef7a0aff32f674eece951
JS0909 <zejace3@gmail.com>
2022-09-05 09:56:10 +0900

    commit

--- tf114/tf19_dropout.py
+++ tf114/tf19_dropout.py
@@ -9,7 +9,10 @@ b1 = tf.compat.v1.Variable(tf.random_normal([30], name = 'bias1'))
 hidden_layer1 = tf.compat.v1.sigmoid(tf.matmul(x, w1) + b1)
 # model.add(Dense(30, input_shape=(2,), activateion='sigmoid'))
 
+dropout_layers = tf.compat.v1.nn.dropout(hidden_layer1, keep_prob=0.7) # 70% keep
+dropout_layers = tf.compat.v1.nn.dropout(hidden_layer1, rate=0.3) # 30% drop
 
-
-
+print(hidden_layer1) # Tensor("Sigmoid:0", shape=(?, 30), dtype=float32)
+# 레이어 하나하나 볼 수 있음
+print(dropout_layers) # Tensor("dropout/mul_1:0", shape=(?, 30), dtype=float32)
 
