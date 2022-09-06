d7e759a377899486578d95b161bb93a64fa8c3a5
JS0909 <zejace3@gmail.com>
2022-09-06 11:51:20 +0900

    commit

--- keras2/keras55_1_hyperParameters.py
+++ keras2/keras55_1_hyperParameters.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 import numpy as np
 from torch import dropout
 from tensorflow.keras.datasets import mnist
@@ -37,11 +38,31 @@ def create_hyperparameter():
     optimizers = ['adam', 'rmsprop', 'adadelta']
     dropout = [0.3, 0.4, 0.5]
     activation = ['relu', 'linear', 'sigmoid', 'selu', 'elu']
+    
     return {'batch_size':batchs, 'optimizer':optimizers, 
             'drop':dropout, 'activation':activation}
     
 hyperparameters = create_hyperparameter()
 
-from sklearn.model_selection import GridSearchCV
-model = GridSearchCV(build_model(), hyperparameters, cv=3)
-model.fit(x_train, y_train, verbose=1)
+
+from tensorflow.python.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+import time
+
+keras_model = KerasClassifier(build_fn=build_model, verbose=1)
+
+model = RandomizedSearchCV(keras_model, hyperparameters, cv=3, n_iter=5)
+
+start = time.time()
+model.fit(x_train, y_train, epochs=7, validation_split=0.2)
+end = time.time()-start
+
+print('걸린 시간: ', end)
+print('model.best_params: ', model.best_params_)
+print('model.best_estimator: ', model.best_estimator_)
+print('model.best_score: ', model.best_score_)
+print('model.score: ', model.score)
+
+from sklearn.metrics import accuracy_score
+y_pred = model.predict(x_test)
+print('acc score: ', accuracy_score(y_test, y_pred))
