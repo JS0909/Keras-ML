274c69783b729e03d5be294b6e8841f89536e3af
JS0909 <zejace3@gmail.com>
2022-09-20 11:22:28 +0900

    c

--- torch/torch12_DataLoader12_kaggle_house.py
+++ torch/torch12_DataLoader12_kaggle_house.py
@@ -222,7 +222,7 @@ train_set = TensorDataset(x_train, y_train)
 test_set = TensorDataset(x_test, y_test)
 
 train_loader = DataLoader(train_set, batch_size=100, shuffle=True)
-test_loader = DataLoader(test_set, batch_size=100, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=100, shuffle=False)
 
 # 2. model
 class Model(nn.Module): # 상속은 상위 클래스만 넣을 수 있음
