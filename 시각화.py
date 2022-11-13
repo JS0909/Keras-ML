5b7a0321d713f655f62ee6005f57427887c5f4e1
JS0909 <zejace3@gmail.com>
2022-11-13 23:33:08 +0900

    commit

---
+++ 시각화.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('Weekly_U.S.Diesel_Retail_Prices.csv')
+# print(data.head())
+
+data['Week of'] = pd.to_datetime(data['Week of'])
+data = data.sort_values(by='Week of', ascending=True, ignore_index=True)
+# print(data.head())
+
+data['year'] = data['Week of'].dt.strftime('%Y')
+data['month'] = data['Week of'].dt.strftime('%m')
+print(data.head())
+
+
+sns.lineplot(data=data, x='year', y='Series ID: PET.EMD_EPD2D_PTE_NUS_DPG.W Dollars per Gallon')
+plt.show()
+
+sns.lineplot(data=data, x='month', y='Series ID: PET.EMD_EPD2D_PTE_NUS_DPG.W Dollars per Gallon')
+plt.show()
