b603e04b7a0906d6f3d88399fc926478f99e8459
JS0909 <zejace3@gmail.com>
2022-12-05 10:08:44 +0900

    commit

---
+++ ml/m59_vif.py
@@ -0,0 +1,6 @@
+# 다중공선성
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+
